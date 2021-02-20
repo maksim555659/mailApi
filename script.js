0d51a4f421f7855bde8a5829784414264ab98dba
maksim555659 <555659@inbox.ru>
2021-02-21 01:40:46 +0300

    test

--- script.js
+++ script.js
@@ -6,14 +6,13 @@ function login(){
     mailru.connect.login();
   
 }
-if(true){
     mailru.loader.require('api', function(){
         mailru.connect.init('783362', '7220a61182dfa95a75746551733ed1f1');
         // mailru.common.users.getInfo(function(result) {
         //     console.log(result);
         //   });
     });
-}
+
 
 
 
