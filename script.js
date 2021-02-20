368e5331d229ec6b838d2a26b8885287f5b42f8f
maksim555659 <555659@inbox.ru>
2021-02-21 00:56:43 +0300

    test

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
-var check = null;
-
-let button = document.getElementById('button');
-
-var user_list = [];
-
-button.onclick = function data(){
     mailru.loader.require('api', function(){
         mailru.connect.init('783362', '7220a61182dfa95a75746551733ed1f1');
-        mailru.common.users.getInfo(function(user_list) {
-            alert(user_list[0].first_name); // выведет имя пользователя с uid 123
-          }, '2363859530532777729');
+        mailru.common.users.getInfo(function(result) {
+            console.log(result);
+          });
     });
 };
 
 
 
-console.log('version 0.4');
+console.log('version 0.5');
 
