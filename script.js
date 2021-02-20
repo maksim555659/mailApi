fdc2beacad6fdf81b21b7e73fe5321a0e3fa7d91
maksim555659 <555659@inbox.ru>
2021-02-21 01:32:16 +0300

    test

--- script.js
+++ script.js
@@ -3,15 +3,16 @@ let button = document.getElementById('auth');
 button.onclick = login;
 
 function login(){
-    mailru.connect.login();
-}
-
- mailru.loader.require('api', function(){
+    mailru.loader.require('api', function(){
+        mailru.connect.login();
         mailru.connect.init('783362', '7220a61182dfa95a75746551733ed1f1');
         // mailru.common.users.getInfo(function(result) {
         //     console.log(result);
         //   });
     });
+}
+
+
 
 
 
