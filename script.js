512fb515956419f70dc77752c7372a26ed33b1ca
maksim555659 <555659@inbox.ru>
2021-02-21 11:58:57 +0300

    test

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
+mailru.loader.require('api', function () {
+    mailru.connect.init('783362', '7220a61182dfa95a75746551733ed1f1');
+});
+
 let authButton = document.getElementById('auth');
 let deAuthButton = document.getElementById('deAuth');
 
 authButton.onclick = login;
 deAuthButton.onclick = leave;
 
-function login(){
-    mailru.events.listen(mailru.connect.events.login, function(session) {
+function login() {
+    mailru.events.listen(mailru.connect.events.login, function (session) {
         // эта функция будет вызвана при логине
-        console.log('session stuff', session.ext_perm); 
-        mailru.connect.getLoginStatus(function(session){
-                 console.log('is auth',session.ext_perm); 
-               });
-      });
-      mailru.connect.login(['widget', 'photos']);
+        console.log('session stuff', session.ext_perm);
+        mailru.connect.getLoginStatus(function (session) {
+            console.log('is auth', session.ext_perm);
+        });
+        if (mailru.session) {
+            mailru.common.users.getInfo(function (result) {
+                console.log(result);
+            });
+        }
+
+    });
+    mailru.connect.login(['widget', 'photos']);
 }
 
-function leave(){
+function leave() {
     mailru.connect.logout();
 }
-    mailru.loader.require('api', function(){
-        mailru.connect.init('783362', '7220a61182dfa95a75746551733ed1f1');
-    });
-
-    if(mailru.session){
-        mailru.common.users.getInfo(function(result) {
-        console.log(result);
-      });
-    }
 
 console.log('version 0.6');
