572ece0931921b2dfd59985797e98c5bbc039fa3
maksim555659 <555659@inbox.ru>
2021-02-21 11:49:48 +0300

    new version

--- script.js
+++ script.js
@@ -20,9 +20,11 @@ function leave(){
 }
     mailru.loader.require('api', function(){
         mailru.connect.init('783362', '7220a61182dfa95a75746551733ed1f1');
-        // mailru.common.users.getInfo(function(result) {
-        //     console.log(result);
-        //   });
+        if(mailru.session){
+            mailru.common.users.getInfo(function(result) {
+            console.log(result);
+          });
+        }
     });
 
 console.log('version 0.6');
